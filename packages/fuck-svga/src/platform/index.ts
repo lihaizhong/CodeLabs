000f9d5518e77d8a1b05ed2fc44eb4c91e15d931
sky <854323752@qq.com>
2025-03-28 01:45:07 +0800

    fix: 整理platform

--- packages/fuck-svga/src/platform/index.ts
+++ packages/fuck-svga/src/platform/index.ts
@@ -1,72 +1,163 @@
-import bridge from "./bridge";
-import env from "./env";
-import ratio from "./ratio";
+const useNow = () => {
+  // performance可以提供更高精度的时间测量，且不受系统时间的调整（如更改系统时间或同步时间）的影响
+  if (typeof performance !== "undefined") {
+    return () => performance.now();
+  }
 
-export enum SE {
-  WECHAT = "weapp",
-  ALIPAY = "alipay",
-  DOUYIN = "tt",
-  H5 = "h5",
-}
+  return () => Date.now();
+};
 
-export class Platform {
-  public global = {
-    env: null,
+export const noop: () => any = () => {};
+
+export class Platform implements SVGAPlatform {
+  private plugins: (() => void)[] = [];
+
+  public global: SVGAPlatformGlobal = {
+    env: SupportedEnv.UNKNOWN,
     br: null,
     fsm: null,
     dpr: 1,
-    sys: null,
+    sys: "UNKNOWN",
   };
 
-  public env = {
-    is: (env: SE) => false,
-    not: (env: SE) => false,
-    get: () => SE.WECHAT,
-    set: () => {},
-  };
+  public noop = noop;
 
-  public remote = {
-    is: () => false,
-    read: () => Promise.reject(),
-  };
+  public now = useNow();
 
-  public local = {
-    write: () => Promise.reject(),
-    read: () => Promise.reject(),
-    remove: () => Promise.reject(),
-  };
+  public local = {} as SVGAPlatform["local"];
 
-  public decode = {
-    toBitmap: () => Promise.reject(),
-    toBase64: () => Promise.reject(),
-  };
+  public remote = {} as SVGAPlatform["remote"];
 
-  public image = {
-    create: () => Promise.reject(),
-    load: () => Promise.reject(),
-    isImage: () => false,
-    isImageBitmap: () => false,
-  };
+  public decode = {} as SVGAPlatform["decode"];
 
-  private plugins = [env, bridge, ratio];
+  public image = {} as SVGAPlatform["image"];
+
+  public rAF = noop as SVGAPlatform["rAF"];
+
+  public getCanvas = noop as SVGAPlatform["getCanvas"];
+
+  public getOfsCanvas = noop as SVGAPlatform["getOfsCanvas"];
 
   constructor() {
+    this.global.env = this.autoEnv();
     this.init();
   }
 
   private init() {
+    this.global.br = this.useBridge();
+    this.global.dpr = this.usePixelRatio();
+    this.global.fsm = this.useFileSystemManager();
+    this.global.sys = this.useSystem().toLocaleLowerCase();
+    this.usePlugins();
+  }
+
+  private autoEnv() {
+    // FIXME：由于抖音场景支持wx对象，所以需要放在wx对象之前检查
+    if (typeof window !== "undefined") {
+      return SupportedEnv.H5;
+    }
+
+    if (typeof tt !== "undefined") {
+      return SupportedEnv.DOUYIN;
+    }
+
+    if (typeof my !== "undefined") {
+      return SupportedEnv.ALIPAY;
+    }
+
+    if (typeof wx !== "undefined") {
+      return SupportedEnv.WECHAT;
+    }
+
+    throw new Error("Unsupported app");
+  }
+
+  private useBridge() {
+    switch (this.global.env) {
+      case SupportedEnv.H5:
+        return globalThis;
+      case SupportedEnv.ALIPAY:
+        return my;
+      case SupportedEnv.DOUYIN:
+        return tt;
+      case SupportedEnv.WECHAT:
+        return wx;
+      default:
+    }
+
+    return {};
+  }
+
+  private usePixelRatio() {
+    const { env, br } = this.global;
+
+    if (env === SupportedEnv.H5) {
+      return globalThis.devicePixelRatio;
+    }
+
+    if ("getWindowInfo" in br) {
+      return (br as any).getWindowInfo().pixelRatio;
+    }
+
+    if ("getSystemInfoSync" in br) {
+      return (br as WechatMiniprogram.Wx).getSystemInfoSync().pixelRatio;
+    }
+
+    return 1;
+  }
+
+  private useFileSystemManager() {
+    const { br } = this.global;
+
+    if ("getFileSystemManager" in br) {
+      return (br as WechatMiniprogram.Wx).getFileSystemManager();
+    }
+
+    return null;
+  }
+
+  private useSystem() {
+    const { env, br } = this.global;
+
+    if (env === SupportedEnv.H5) {
+      const UA = navigator.userAgent;
+
+      if (/(Android)/i.test(UA)) {
+        return "Android";
+      }
+
+      if (/(iPhone|iPad|iPod|iOS)/i.test(UA)) {
+        return "iOS";
+      }
+
+      if (/(OpenHarmony|ArkWeb)/i.test(UA)) {
+        return "OpenHarmony";
+      }
+    } else {
+      if (env === SupportedEnv.ALIPAY) {
+        return (br as any).getDeviceBaseInfo().platform as string;
+      }
+
+      if (env === SupportedEnv.DOUYIN) {
+        return (br as any).getDeviceInfoSync().platform as string;
+      }
+
+      if (env === SupportedEnv.WECHAT) {
+        return (br as any).getDeviceInfo().platform as string;
+      }
+    }
+
+    return "UNKNOWN";
+  }
+
+  private usePlugins() {
     this.plugins.forEach((plugin) => {
-      plugin.install.call(null, this.global, this.env, SE);
+      plugin.call(this);
     });
   }
 
-  rAF(canvas: WechatMiniprogram.Canvas, callback: FrameRequestCallback) {
-    (
-      (this.env.is(SE.H5) ? globalThis : canvas) as WechatMiniprogram.Canvas
-    ).requestAnimationFrame(callback);
+  public setEnv(env: SupportedEnv) {
+    this.global.env = env;
+    this.init();
   }
-
-  getCanvas() {}
-
-  getOfsCanvas() {}
 }
---
+++ packages/fuck-svga/typings/platform.d.ts
@@ -0,0 +1,59 @@
+enum SupportedEnv {
+  WECHAT = "weapp",
+  ALIPAY = "alipay",
+  DOUYIN = "tt",
+  H5 = "h5",
+  UNKNOWN = "unknown",
+}
+
+interface SVGAPlatformGlobal {
+  env: SupportedEnv;
+  br: any;
+  fsm: any;
+  dpr: number;
+  sys: string;
+}
+
+interface SVGAPlatform {
+  global: SVGAPlatformGlobal;
+
+  noop: () => any;
+
+  now: () => number;
+
+  remote: {
+    is: () => boolean;
+    read: () => Promise<ArrayBuffer>;
+  };
+
+  local: {
+    write: () => Promise<void>;
+    read: () => Promise<ArrayBuffer>;
+    remove: () => Promise<void>;
+  };
+
+  decode: {
+    toBitmap: () => Promise<ImageBitmap>;
+    toBase64: () => Promise<string>;
+  };
+
+  image: {
+    create: () => Promise<PlatformImage>;
+    load: () => Promise<void>;
+    isImage: () => boolean;
+    isImageBitmap: () => boolean;
+  };
+
+  rAF: (canvas: WechatMiniprogram.Canvas, callback: () => void) => void;
+
+  getCanvas: (
+    selector: string,
+    component?: WechatMiniprogram.Component.TrivialInstance | null
+  ) => Promise<IGetCanvasResult>;
+
+  getOfsCanvas: (
+    options: WechatMiniprogram.CreateOffscreenCanvasOption
+  ) => IGetOffscreenCanvasResult;
+
+  setEnv: (env: SupportedEnv) => void;
+}
