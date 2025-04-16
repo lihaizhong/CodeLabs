23c52f4f919b029dc38c9d269b410709a25fb50f
lihzsky <lihaizh_cn@foxmail.com>
2025-04-16 21:57:52 +0800

    fix: 网络写入缓存失败

--- packages/fuck-svga/src/platform/plugins/plugin-download.ts
+++ packages/fuck-svga/src/platform/plugins/plugin-download.ts
@@ -26,21 +26,31 @@ export default definePlugin<"remote">({
       };
     }
 
+    function download(url: string, enableCache: boolean): Promise<ArrayBuffer> {
+      return new Promise<ArrayBuffer>((resolve, reject) => {
+        (br as WechatMiniprogram.Wx).request({
+          url,
+          // @ts-ignore 支付宝小程序必须有该字段
+          dataType: "arraybuffer",
+          responseType: "arraybuffer",
+          enableCache,
+          success: (res: any) => resolve(res.data as ArrayBuffer),
+          fail: reject,
+        });
+      }).catch((err) => {
+        const errorMessage = err.errMsg || err.errorMessage || err.message;
+        // FIXME: 可能存在写入网络缓存空间失败的情况，此时重新下载
+        if (errorMessage.includes("ERR_CACHE_WRITE_FAILURE") || errorMessage.includes("ERR_CACHE_WRITE_FAILED")) {
+          return download(url, false);
+        }
+
+        throw err;
+      });
+    }
+
     return {
       is: isRemote,
-      fetch: (url: string) =>
-        new Promise((resolve, reject) => {
-          (br as WechatMiniprogram.Wx).request({
-            url,
-            // @ts-ignore 支付宝小程序必须有该字段
-            dataType: "arraybuffer",
-            responseType: "arraybuffer",
-            enableCache: true,
-            // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-            success: (res: any) => resolve(res.data as ArrayBuffer),
-            fail: reject,
-          });
-        }),
+      fetch: (url: string) => download(url, true),
     };
   },
 });
