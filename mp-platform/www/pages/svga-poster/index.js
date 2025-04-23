977ee75d709492507d5d06244c6337893189c529
lihzsky <lihaizh_cn@foxmail.com>
2025-04-23 14:59:58 +0800

    svga文件分析

--- mp-platform/www/pages/svga-poster/index.js
+++ mp-platform/www/pages/svga-poster/index.js
@@ -1,10 +1,28 @@
-import { Parser, Poster } from "octopus-svga";
+import "weui";
+import { Parser, Poster, platform } from "octopus-svga";
 import { posterFiles } from "../../utils/constants";
 
 const parser = new Parser();
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const posterItem = await parser.load(posterFiles[0]);
+async function analyzeSpriteImages(url) {
+  const $analyze = document.getElementById("analyze-container");
+
+  const videoItem = await parser.load(url);
+
+  $analyze.innerHTML = "";  
+  for (const key in videoItem.images) {
+    const image = videoItem.images[key];
+    const $image = document.createElement("img");
+
+    $image.src = platform.decode.toDataURL(image);
+    $image.style.width = "100%";
+    $image.style.objectFit = "scale-down";
+    $analyze.appendChild($image);
+  }
+}
+
+async function generatePoster(url) {
+  const posterItem = await parser.load(url);
   const { width, height } = posterItem.size;
   const poster = new Poster(width, height);
 
@@ -12,13 +30,38 @@ window.addEventListener("DOMContentLoaded", async () => {
   console.log("posterItem", posterItem);
   await poster.mount(posterItem);
 
-  const $elem = document.createElement("img");
+  const $elem = document.getElementById("poster");
 
   poster.setContentMode("aspect-fit");
   poster.draw();
 
   $elem.src = poster.toDataURL();
-  $elem.style.width = "100%";
   console.log("data url", $elem.src);
-  document.body.appendChild($elem);
+  analyzeSpriteImages(url);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const len = posterFiles.length;
+  let current = 0;
+
+  const $prevBtn = document.getElementById("prev-btn");
+  const $nextBtn = document.getElementById("next-btn");
+
+  $prevBtn.addEventListener("click", async () => {
+    if (current - 1 < 0) {
+      return;
+    }
+
+    generatePoster(posterFiles[--current]);
+  });
+
+  $nextBtn.addEventListener("click", async () => {
+    if (current + 1 > len) {
+      return;
+    }
+
+    generatePoster(posterFiles[++current]);
+  });
+
+  generatePoster(posterFiles[current]);
 });
