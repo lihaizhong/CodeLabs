6b3e1866ce31f4712148d9df9c35641c183fad6b
lihzsky <lihaizh_cn@foxmail.com>
2025-04-24 10:37:10 +0800

    feat: 测试svga海报

--- mp-platform/www/utils/constants.js
+++ mp-platform/www/utils/constants.js
@@ -88,7 +88,9 @@ export const posterFiles = [
   // "支付宝商户授权码（透明色块）.svga",
   // "测试.svga",
   // "测试1.svga",
-  "测试2.svga"
+  // "测试2.svga"
+  "支付宝授权码（白色）.svga",
+  "支付宝授权码（透明）.svga",
 ].map((url) => `${REPLACE_SVGA_URL}/poster/${url}`);
 
 export function getOneAtRandom(files) {
