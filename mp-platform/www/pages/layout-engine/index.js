5c8efde78360dacd8b1214d4bfa99e3c7c1059c4
lihzsky <lihaizh_cn@foxmail.com>
2025-04-27 19:56:16 +0800

    fix: 编写测试

--- mp-platform/www/pages/layout-engine/index.js
+++ mp-platform/www/pages/layout-engine/index.js
@@ -1,5 +1,22 @@
 import "weui";
+import { LayoutEngine, LayoutNode, CanvasRenderer } from "octopus-layout-engine";
 
-window.addEventListener("load", () => {
-  
+window.addEventListener("DOMContentLoaded", () => {
+  const $palette = document.getElementById("palette");
+  const layoutEngine = new LayoutEngine({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    devicePixelRatio: window.devicePixelRatio,
+  });
+  const layoutRenderer = new CanvasRenderer($palette, window.devicePixelRatio);
+  const node = new LayoutNode({
+    type: "text",
+    content: "Hello World",
+    style: {
+      fontSize: 16,
+      color: "#FFFFFF"
+    },
+  });
+
+  layoutEngine.setRenderer(layoutRenderer);
 })
