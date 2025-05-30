9c5c8fefa50b05c5deae95c7adfcc26333af4d93
lihzsky <lihaizh_cn@foxmail.com>
2025-05-30 14:14:06 +0800

    fix: 更新依赖

--- packages/octopus-svga/dist/src/extensions/svga-decoder/io/OptimizedReader.d.ts
+++
@@ -1,9 +0,0 @@
-import Reader from "./Reader";
-/**
- * 对于固定结构的protobuf消息，可以预计算字段的位置，避免每次都需要解析tag
- */
-export declare class OptimizedReader extends Reader {
-    private static fieldPositionCache;
-    cacheFieldPositions(messageId: string): Map<number, number[]>;
-    jumpToField(messageId: string, fieldNumber: number, occurrence?: number): boolean;
-}
