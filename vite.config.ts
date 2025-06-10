4ac0ddf91813f1f0ec8f28c18dfa2123491762f0
lihzsky <lihaizh_cn@foxmail.com>
2025-06-11 03:06:37 +0800

    chore: 使用vite构建项目

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,63 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-// import glob from "tinyglobby";
+import { globSync } from "tinyglobby";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __root = path.resolve(__dirname, "mp-platform/www");
+const findAllEntries = () => {
+  const entries = globSync("pages/**/index.html", {
+    cwd: __root,
+    absolute: true,
+    onlyFiles: true
+  });
+
+  const inputEntries = entries.reduce((entries, entry) => {
+    // pages/index/index.html => pages/index
+    const dirname = path.dirname(entry);
+    // pages/index => index
+    let basename = path.basename(dirname);
+
+    if (basename === "pages") {
+      // pages/index/index.html => index
+      basename = path.basename(entry, '.html')
+      entries["index"] = entry;
+    }
+
+    entries[basename] = entry;
+
+    return entries;
+  }, {});
+
+  // console.log("inputEntries", inputEntries);
+
+  return inputEntries;
+}
 
 export default defineConfig({
-  root: "./mp-platform/www",
+  root: __root,
 
-  publicDir: "../../public",
+  publicDir: path.resolve(__dirname, "public"),
+
+  resolve: {
+    alias: {
+      "@utils": path.resolve(__dirname, "mp-platform/www/utils"),
+    },
+  },
+
+  css: {
+    transformer: "lightningcss",
+  },
+
+  appType: "mpa",
+
+  build: {
+    rollupOptions: {
+      input: findAllEntries(),
+    }
+  },
+
+  server: {
+    open: "/pages/index.html"
+  }
 });
