f586f80a4a47947f16fd124c7c706ec36a48a786
lihzsky <lihaizh_cn@foxmail.com>
2025-07-21 23:28:54 +0800

    fix: 优化核心知识库

--- packages/you-need-suggest/src/calculations/pinyin.ts
+++ packages/you-need-suggest/src/calculations/pinyin.ts
@@ -2,6 +2,11 @@ import { defineAdaptor } from "../defineAdaptor";
 
 /**
  * https://github.com/sxei/pinyinjs
+ * https://www.cnblogs.com/guohu/p/5021336.html
+ * 
+ * - [打造最好的Java拼音库TinyPinyin（一）：单字符转拼音的极致优化](https://github.com/promeG/promeg.github.io/blob/master/_posts/2017-03-19-tinypinyin-part-1.markdown)
+ * - [打造最好的Java拼音库TinyPinyin（二）：多音字快速处理方案](https://github.com/promeG/promeg.github.io/blob/master/_posts/2017-03-20-tinypinyin-part-2.markdown)
+ * - [打造最好的Java拼音库TinyPinyin（三）：API设计和测试实践](https://github.com/promeG/promeg.github.io/blob/master/_posts/2017-03-22-tinypinyin-part-3.markdown)
  */
 class PinYinCalculator {
   private options: any;
