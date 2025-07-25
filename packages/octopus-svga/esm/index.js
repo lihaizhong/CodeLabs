a9b207e3c84f811518eceff10cb31bafba6ad058
lihzsky <lihaizh_cn@foxmail.com>
2025-07-25 11:57:36 +0800

    chore: build

--- packages/octopus-svga/esm/index.js
+++ packages/octopus-svga/esm/index.js
@@ -23,7 +23,7 @@ class EnhancedPlatform extends OctopusPlatform {
             pluginNow,
             pluginPath,
             pluginRAF,
-        ], "1.2.0");
+        ], "1.2.1");
         this.init();
     }
     installPlugin(plugin) {
