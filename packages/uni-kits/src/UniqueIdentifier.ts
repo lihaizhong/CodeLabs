fad07ac48ad07908e68af3953bb2f153304f2f26
lihzsky <lihaizh_cn@foxmail.com>
2025-10-24 14:36:52 +0800

    fix: 优化id生成器

--- packages/uni-kits/src/UniqueIdentifier.ts
+++ packages/uni-kits/src/UniqueIdentifier.ts
@@ -1,10 +1,9 @@
 export function genUniqueIdentifier(): string {
-  let d = Date.now()
+  let seed = Date.now()
 
   return 'xxxx-xxxx-6xxx-yxxx-xxxx'.replace(/[xy]/g, (c) => {
-    const r = (d + Math.random() * 16) % 16 | 0
-
-    d = Math.floor(d / 16)
+    const r = (seed + Math.random() * 16) % 16 | 0
+    seed = Math.floor(seed / 16)
 
     return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
   })
